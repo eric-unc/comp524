1e0312edabfef49aeea135065defc72d31be84af
Eric Schneider <u1643364@live.unc.edu>
2021-02-06 12:48:21 -0500

    Token helper functions seem good now

--- a2/a2.rkt
+++ a2/a2.rkt
@@ -17,19 +17,22 @@
       [(";") 'SEMICOLON]
       [(".") 'PERIOD])))
 
-; TODO: int v. float
 (define (number-token str)
-  (token 'NUM (string->number str)))
+  (if (string-contains? str ".")
+      (token 'FLOAT (string->number str))
+      (token 'INT (string->number str))))
 
-; TODO: get rid of ", "
 (define (string-token str)
-  (token 'STRING str))
+  (token 'STRING (substring str 1 (- (string-length str) 1))))
 
 (define (name-or-keyword-token str)
   (case str
     [("def" "fun", "if", "not", "and", "or")
      (token (string->symbol (string-upcase (string-trim str))))]
     [else (token 'NAME (string->symbol str))]))
+
+(define (invalid-token str)
+  (token 'INVALID str))
 
 ; Matching table
 (define re-table
@@ -38,9 +41,9 @@
     (list #px"^//[^\n]+(\n|$)" skip-match) ; // comments
     (list #px"^/\\*(?:.|\n)*\\*/" skip-match) ; /* comments */
     (list #px"^[(){},;.]" punctuation-token)
-    (list #px"^-?\\d+(?:\\.\\d+)?" number-token)
-    (list #px"^\".*\"" string-token)
-    (list #px"^[^(){},;.\" \r\n\t\\d][^(){},;.\" \r\n\t]*" name-or-keyword-token)))
+    (list #px"^-?\\d+(?:\\.\\d+)?(?=[\r\n\t (){},;.]|$)" number-token)
+    (list #px"^\".*\"(?=[\r\n\t (){},;.]|$)" string-token)
+    (list #px"^[^(){},;.\" \r\n\t\\d][^(){},;.\" \r\n\t]*(?=[\r\n\t (){},;.]|$)" name-or-keyword-token)))
 
 ; Lex function
 (define (lex str) null)
