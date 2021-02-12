cbbfd51599262661183a10495c3c7071f6180eb7
Eric Schneider <u1643364@live.unc.edu>
2021-02-11 20:56:22 -0500

    close to a3 but i don't understand tokens

--- a3/a3.rkt
+++ a3/a3.rkt
@@ -2,4 +2,64 @@
 
 (require (only-in (file "lex.rkt") lex))
 
-; TODO
+(define tokens (make-parameter null))
+
+(define (parse-program)
+  (list 'program
+        (parse-expr-list)))
+
+(define (parse-expr-list)
+  (list 'exprList
+        (parse-expr)
+        (parse-opt-expr-list)))
+
+(define (parse-opt-expr-list)
+  (if (empty? tokens)
+      (list 'optExprList)
+      (list 'optExprList (parse-expr-list))))
+
+(define (parse-expr)
+  (list
+   'expr
+  (if (check 'OPAREN)
+      (parse-invocation)
+      (parse-atom))))
+
+(define (parse-atom)
+  (list 'atom
+        (if (check 'NAME)
+            (consume 'NAME)
+            (if (check 'STRING)
+                (consume 'STRING)
+                (parse-number)))))
+
+(define (parse-number)
+  (list 'number
+        (if (check 'INT)
+            (consume 'INT)
+            (consume 'FLOAT))))
+
+(define (parse-invocation)
+  (list 'invocation
+        (consume 'OPAREN)
+        (parse-expr-list)
+        (consume 'CPAREN)))
+
+(define (check type)
+  (if (empty? (tokens))
+      #f
+      (equal? type (first (first (tokens))))))
+
+(define (consume type)
+  (when (empty? (tokens))
+    (error (~a "expected token of type " type " but no remaining tokens")))
+  (let ([token (first (tokens))])
+    (when (not (equal? type (first token)))
+      (error (~a "expected token of type " type " but actual token was " token)))
+    (tokens (rest (tokens)))  ; update tokens: remove first token
+    token))
+
+; TODO https://piazza.com/class/kjvqyzyra2out?cid=69
+(define (parse code)
+  (parameterize ([tokens (lex code)])
+    (parse-program)))
