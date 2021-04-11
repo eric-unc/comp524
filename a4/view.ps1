bac3b86b1996a26406555e0a4f2ee2fb76c59972
Eric Schneider <u1643364@live.unc.edu>
2021-04-11 01:10:10 -0400

    Finished a08, should be interesting.

--- a4/view.ps1
+++ a4/view.ps1
@@ -1,1 +1,2 @@
+# Grip can be installed here -> https://github.com/joeyespo/grip
 grip 'Code as Data Lua and Object-Orientation as a Case Study.md'
--- a8/view.ps1
+++ a8/view.ps1
@@ -1,1 +1,2 @@
-grip 'Against Object-Orientation.md'
+# Grip can be installed here -> https://github.com/joeyespo/grip
+grip 'Object-Orientation, Encapsulation, and Other Things That Are Wrong With Society.md'
